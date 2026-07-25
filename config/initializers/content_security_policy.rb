643ce9f5d6259069ee22cd4ddc12fb2d37f2d652
Cesar Escobar <cesarsilvaescobar@gmail.com>
2026-07-25 17:34:01 -0300

    security: add content security policy baseline

---
+++ config/initializers/content_security_policy.rb
@@ -0,0 +1,17 @@
+Rails.application.configure do
+  config.content_security_policy do |policy|
+    policy.default_src :self
+    policy.font_src :self, :data
+    policy.img_src :self, :data
+    policy.object_src :none
+    policy.script_src :self
+    policy.style_src :self
+    policy.connect_src :self
+    policy.frame_ancestors :none
+    policy.base_uri :self
+    policy.form_action :self
+  end
+
+  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+  config.content_security_policy_nonce_directives = %w[script-src]
+end
