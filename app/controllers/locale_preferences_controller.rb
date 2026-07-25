fa719514ed3cfcaf3cc13524e849a0e5009c38c6
Cesar Escobar <cesarsilvaescobar@gmail.com>
2026-07-25 20:44:46 -0300

    fix: interpolate locale display name safely

--- app/controllers/locale_preferences_controller.rb
+++ app/controllers/locale_preferences_controller.rb
@@ -8,6 +8,7 @@ class LocalePreferencesController < ApplicationController
     end
 
     Current.user.update!(locale: locale)
-    redirect_back fallback_location: root_path, notice: I18n.t("localization.updated", locale: Localization::SupportedLocales.fetch(locale).label)
+    locale_name = Localization::SupportedLocales.fetch(locale).label
+    redirect_back fallback_location: root_path, notice: I18n.t("localization.updated", locale_name: locale_name)
   end
 end
