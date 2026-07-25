628edabd0e5a1fcb142838b2cae2e90c7d1ab501
Cesar Escobar <cesarsilvaescobar@gmail.com>
2026-07-25 20:17:04 -0300

    test: cover locale preference persistence

---
+++ test/controllers/locale_preferences_controller_test.rb
@@ -0,0 +1,32 @@
+require "test_helper"
+
+class LocalePreferencesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = User.create!(email_address: "locale@example.com", password: "a-secure-password", locale: "en", time_zone: "UTC")
+
+    get new_session_path
+    token = Nokogiri::HTML(response.body).at_css("input[name='authenticity_token']")["value"]
+    post session_path, params: {
+      authenticity_token: token,
+      email_address: @user.email_address,
+      password: "a-secure-password"
+    }
+    follow_redirect!
+  end
+
+  test "persists a supported locale" do
+    token = Nokogiri::HTML(response.body).at_css("meta[name='csrf-token']")["content"]
+    patch locale_preference_path, params: { authenticity_token: token, locale: "pt-BR" }, headers: { "HTTP_REFERER" => root_url }
+
+    assert_redirected_to root_url
+    assert_equal "pt-BR", @user.reload.locale
+  end
+
+  test "rejects unsupported locales" do
+    token = Nokogiri::HTML(response.body).at_css("meta[name='csrf-token']")["content"]
+    patch locale_preference_path, params: { authenticity_token: token, locale: "invalid" }, headers: { "HTTP_REFERER" => root_url }
+
+    assert_redirected_to root_url
+    assert_equal "en", @user.reload.locale
+  end
+end
