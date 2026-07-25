1ab11877a84f3f19e3a5f41e62da853f85e97a20
Cesar Escobar <cesarsilvaescobar@gmail.com>
2026-07-25 18:12:22 -0300

    test: align component test namespace

--- test/components/ui/button_component_test.rb
+++ test/components/ui/button_component_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
-class UI::ButtonComponentTest < ViewComponent::TestCase
+class Ui::ButtonComponentTest < ViewComponent::TestCase
   test "renders a link variant" do
-    render_inline UI::ButtonComponent.new(label: "Health", href: "/health")
+    render_inline Ui::ButtonComponent.new(label: "Health", href: "/health")
 
     assert_selector "a.button.button--primary[href='/health']", text: "Health"
   end
