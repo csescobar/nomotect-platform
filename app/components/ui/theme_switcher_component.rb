ef868166563f72610f87bb391e78ab35a247de9a
Cesar Escobar <cesarsilvaescobar@gmail.com>
2026-07-25 18:41:57 -0300

    feat: add accessible theme switcher component

---
+++ app/components/ui/theme_switcher_component.rb
@@ -0,0 +1,19 @@
+module Ui
+  class ThemeSwitcherComponent < ApplicationComponent
+    OPTIONS = %w[system light dark].freeze
+
+    def call
+      tag.div(data: { controller: "theme" }, class: "theme-switcher") do
+        safe_join([
+          tag.label(I18n.t("theme.label"), for: "theme-preference", class: "theme-switcher__label"),
+          tag.select(
+            options_for_select(OPTIONS.map { |theme| [I18n.t("theme.#{theme}"), theme] }),
+            id: "theme-preference",
+            class: "theme-switcher__select",
+            data: { theme_target: "select", action: "theme#change" }
+          )
+        ])
+      end
+    end
+  end
+end
