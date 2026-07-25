a822fe8838c23340f0462f3b4d02ae6e2024210e
Cesar Escobar <cesarsilvaescobar@gmail.com>
2026-07-25 20:13:18 -0300

    feat: validate user localization preferences

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   validates :email_address, presence: true, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 12 }, allow_nil: true
+  validates :locale, inclusion: { in: ->(_) { Localization::SupportedLocales.codes } }
+  validates :time_zone, inclusion: { in: ActiveSupport::TimeZone.all.map(&:tzinfo).map(&:name) }
 
   generates_token_for :password_reset, expires_in: 20.minutes do
     password_salt&.last(10)
