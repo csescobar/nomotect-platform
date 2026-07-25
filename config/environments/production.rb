cfcbfe0d714de4c05cc57bb9acf4e400b005bd4c
Cesar Escobar <cesarsilvaescobar@gmail.com>
2026-07-25 17:33:16 -0300

    build: configure production environment

---
+++ config/environments/production.rb
@@ -0,0 +1,17 @@
+require "active_support/core_ext/integer/time"
+
+Rails.application.configure do
+  config.enable_reloading = false
+  config.eager_load = true
+  config.consider_all_requests_local = false
+  config.action_controller.perform_caching = true
+  config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
+  config.active_storage.service = :local
+  config.force_ssl = true
+  config.log_tags = [:request_id]
+  config.logger = ActiveSupport::TaggedLogging.logger($stdout)
+  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
+  config.active_support.report_deprecations = false
+  config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: ENV.fetch("APP_HOST", "example.com") }
+end
