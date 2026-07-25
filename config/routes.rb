a4a02b80964608c76a16434097ba8366b391a4de
Cesar Escobar <cesarsilvaescobar@gmail.com>
2026-07-25 19:53:33 -0300

    feat: route component showcase

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resource :session, only: %i[new create destroy]
   resources :passwords, param: :token, only: %i[new create edit update]
 
+  resource :component_showcase, only: :show
   get "health", to: "health#show", as: :health
   get "up", to: "rails/health#show", as: :rails_health_check
 
