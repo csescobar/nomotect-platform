29964c1182dda5b4cf3b86cc6d57c3740407bef1
Cesar Escobar <cesarsilvaescobar@gmail.com>
2026-07-25 20:54:43 -0300

    feat: associate users with organizations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
+  has_many :memberships, dependent: :destroy
+  has_many :organizations, through: :memberships
 
   normalizes :email_address, with: ->(email) { email.strip.downcase }
 
