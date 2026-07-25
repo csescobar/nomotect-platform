c18a8553dc9684042f69b7e25cb544d931263f40
Cesar Escobar <cesarsilvaescobar@gmail.com>
2026-07-25 20:12:54 -0300

    feat: add locale switcher component

---
+++ app/components/ui/locale_switcher_component.rb
@@ -0,0 +1,26 @@
+module Ui
+  class LocaleSwitcherComponent < BaseComponent
+    def initialize(current_locale:, action:, html_options: {})
+      @current_locale = Localization::SupportedLocales.fetch(current_locale).code
+      @action = action
+      @html_options = html_options
+    end
+
+    def call
+      form_with(url: @action, method: :patch, html: merged_html_options(class: "locale-switcher")) do |form|
+        safe_join([
+          form.label(:locale, I18n.t("localization.label"), class: "locale-switcher__label"),
+          form.select(
+            :locale,
+            Localization::SupportedLocales.options,
+            { selected: @current_locale },
+            class: "locale-switcher__select",
+            aria: { label: I18n.t("localization.label") },
+            onchange: "this.form.requestSubmit()"
+          ),
+          tag.noscript(form.submit(I18n.t("localization.apply"), class: "button button--secondary button--small"))
+        ])
+      end
+    end
+  end
+end
