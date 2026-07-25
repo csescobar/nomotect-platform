22ac66d1ec8db16230ad88655ce8abcb74432646
Cesar Escobar <cesarsilvaescobar@gmail.com>
2026-07-25 18:14:30 -0300

    fix: use Active Support delegation for current user

--- app/models/current.rb
+++ app/models/current.rb
@@ -1,7 +1,7 @@
 class Current < ActiveSupport::CurrentAttributes
   attribute :session, :request_id, :correlation_id, :locale
 
-  delegates :user, to: :session, allow_nil: true
+  delegate :user, to: :session, allow_nil: true
 
   resets do
     Time.zone = "UTC"
