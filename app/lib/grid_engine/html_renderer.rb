455fdd287ba1dfba2a027e20633b176ceb5f5874
Cesar Escobar <cesarsilvaescobar@gmail.com>
2026-07-25 23:52:51 -0300

    feat: personalize accessible grid columns

--- app/lib/grid_engine/html_renderer.rb
+++ app/lib/grid_engine/html_renderer.rb
@@ -3,18 +3,21 @@ module GridEngine
     include ActionView::Helpers::TagHelper
     include ActionView::Helpers::OutputSafetyHelper
 
-    def initialize(definition, records, locale: I18n.locale)
+    def initialize(definition, records, locale: I18n.locale, columns: nil)
       @definition = definition
       @records = records
       @locale = locale
+      @column_keys = Array(columns).map(&:to_s).presence
     end
 
     def call
-      tag.table(class: "grid-engine-table") do
-        safe_join([
-          tag.thead { tag.tr { safe_join(visible_columns.map { |column| tag.th(label_for(column), scope: "col") }) } },
-          tag.tbody { safe_join(@records.map { |record| row(record) }) }
-        ])
+      tag.div(class: "grid-engine-table-wrap", tabindex: 0) do
+        tag.table(class: "grid-engine-table") do
+          safe_join([
+            tag.thead { tag.tr { safe_join(visible_columns.map { |column| tag.th(label_for(column), scope: "col") }) } },
+            tag.tbody { safe_join(@records.map { |record| row(record) }) }
+          ])
+        end
       end
     end
 
@@ -30,7 +33,12 @@ module GridEngine
       end
     end
 
-    def visible_columns = @definition.columns.values.select(&:visible)
+    def visible_columns
+      defaults = @definition.columns.values.select(&:visible)
+      return defaults unless @column_keys
+
+      @column_keys.filter_map { |key| @definition.columns[key] }.uniq
+    end
 
     def label_for(column)
       column.label || I18n.t("grid_engine.columns.#{column.key}", default: column.key.humanize)
