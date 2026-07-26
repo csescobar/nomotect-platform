f8c80e3263105c86c66d75ab193578d308e01c4f
Cesar Escobar <cesarsilvaescobar@gmail.com>
2026-07-26 18:05:45 -0300

    style: satisfy array spacing convention

--- app/models/privacy_preference.rb
+++ app/models/privacy_preference.rb
@@ -3,7 +3,7 @@ class PrivacyPreference < ApplicationRecord
   belongs_to :user
 
   validates :purpose, presence: true, length: { maximum: 100 }
-  validates :granted, inclusion: { in: [true, false] }
+  validates :granted, inclusion: { in: [ true, false ] }
   validates :decided_at, presence: true
   validates :purpose, uniqueness: { scope: %i[organization_id user_id] }
 end
