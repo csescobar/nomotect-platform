77f446e251203d25a93b2a248b5b6c132aa6f10f
Cesar Escobar <cesarsilvaescobar@gmail.com>
2026-07-26 17:25:57 -0300

    feat: add authenticated tenant selection endpoint

---
+++ app/controllers/tenant_selections_controller.rb
@@ -0,0 +1,8 @@
+class TenantSelectionsController < ApplicationController
+  def update
+    membership = Current.user.memberships.find_by!(organization_id: params.require(:organization_id))
+    session[:active_organization_id] = membership.organization_id
+
+    redirect_back fallback_location: organization_path(membership.organization), notice: I18n.t("tenant_selection.updated")
+  end
+end
