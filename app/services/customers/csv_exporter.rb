27d19becf3b99871bdf2a7411f53d959989cde6e
Cesar Escobar <cesarsilvaescobar@gmail.com>
2026-07-26 09:00:02 -0300

    fix: keep customer exports deterministic

--- app/services/customers/csv_exporter.rb
+++ app/services/customers/csv_exporter.rb
@@ -6,7 +6,7 @@ module Customers
       customers = VisibleTo.new(user: user, organization: organization).call
       CSV.generate(headers: true) do |csv|
         csv << %w[id name email_address status updated_at]
-        customers.find_each do |customer|
+        customers.each do |customer|
           csv << [ customer.id, customer.name, customer.email_address, customer.status, customer.updated_at.iso8601 ]
         end
       end
