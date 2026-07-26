469040cc996423c312332cdcecd76ce9ba9396ec
Cesar Escobar <cesarsilvaescobar@gmail.com>
2026-07-25 21:11:29 -0300

    feat: add organization invitation management

---
+++ app/controllers/organization_invitations_controller.rb
@@ -0,0 +1,48 @@
+class OrganizationInvitationsController < ApplicationController
+  before_action :set_organization, only: %i[create destroy]
+
+  def create
+    authorize!(@organization, :manage_members?)
+    @invitation = @organization.organization_invitations.new(invitation_params.merge(invited_by: Current.user))
+
+    if @invitation.save
+      redirect_to @organization, notice: t("organization_invitations.created")
+    else
+      load_organization_members
+      render "organizations/show", status: :unprocessable_content
+    end
+  end
+
+  def destroy
+    authorize!(@organization, :manage_members?)
+    invitation = @organization.organization_invitations.pending.find(params[:id])
+    invitation.revoke!
+    redirect_to @organization, notice: t("organization_invitations.revoked")
+  end
+
+  def accept
+    invitation = OrganizationInvitation.find_by_acceptance_token!(params[:token])
+    invitation.accept!(Current.user)
+    redirect_to invitation.organization, notice: t("organization_invitations.accepted")
+  rescue ActiveSupport::MessageVerifier::InvalidSignature, ActiveRecord::RecordNotFound
+    redirect_to organizations_path, alert: t("organization_invitations.invalid")
+  rescue ActiveRecord::RecordInvalid
+    redirect_to organizations_path, alert: t("organization_invitations.email_mismatch")
+  end
+
+  private
+
+  def set_organization
+    @organization = Organization.find(params[:organization_id])
+  end
+
+  def invitation_params
+    params.require(:organization_invitation).permit(:email_address, :role)
+  end
+
+  def load_organization_members
+    @membership = @organization.membership_for(Current.user)
+    @memberships = @organization.memberships.includes(:user).order(:created_at)
+    @pending_invitations = @organization.organization_invitations.pending.order(:created_at)
+  end
+end
