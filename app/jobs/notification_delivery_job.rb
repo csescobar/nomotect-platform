9f3f2edb79765690a6d19e63f40e542cbf971fcf
Cesar Escobar <cesarsilvaescobar@gmail.com>
2026-07-26 15:49:39 -0300

    feat: scope notification jobs to tenant identity

--- app/jobs/notification_delivery_job.rb
+++ app/jobs/notification_delivery_job.rb
@@ -1,11 +1,17 @@
 class NotificationDeliveryJob < ApplicationJob
   queue_as :default
 
-  def perform(notification_id)
-    notification = Notification.find(notification_id)
+  def perform(organization_id, notification_id)
+    organization = Organization.find(organization_id)
+    notification = organization.notifications.find(notification_id)
+    TenantBoundary.assert_membership!(organization: organization, user: notification.recipient)
     return if notification.status == "delivered"
 
-    ActiveSupport::Notifications.instrument("notification.deliver.enterprise_services", notification_id: notification.id) do
+    ActiveSupport::Notifications.instrument(
+      "notification.deliver.enterprise_services",
+      organization_id: organization.id,
+      notification_id: notification.id
+    ) do
       notification.update!(status: "delivered", delivered_at: Time.current)
     end
   rescue StandardError
