4c63222e67b07fbef02521d3bfc5d75f3ec3a4c3
Cesar Escobar <cesarsilvaescobar@gmail.com>
2026-07-26 09:51:27 -0300

    feat: establish active tenant request context

--- app/models/current.rb
+++ app/models/current.rb
@@ -1,7 +1,12 @@
 class Current < ActiveSupport::CurrentAttributes
-  attribute :session, :request_id, :correlation_id, :locale
+  attribute :session, :request_id, :correlation_id, :locale, :organization, :membership
 
   delegate :user, to: :session, allow_nil: true
+
+  def organization=(organization)
+    super
+    self.membership = user&.memberships&.find_by(organization: organization)
+  end
 
   resets do
     Time.zone = "UTC"
