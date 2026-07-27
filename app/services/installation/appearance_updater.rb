14a0a064857db5f4c1396d648ef8c170c45b0b27
Cesar Escobar <cesarsilvaescobar@gmail.com>
2026-07-27 10:33:20 -0300

    feat: orchestrate installation appearance updates

---
+++ app/services/installation/appearance_updater.rb
@@ -0,0 +1,36 @@
+module Installation
+  class AppearanceUpdater
+    def initialize(store: AppearanceStore.new, branding_store: BrandingStore.new, token_installer: DesignTokenInstaller.new)
+      @store = store
+      @branding_store = branding_store
+      @token_installer = token_installer
+    end
+
+    def update!(attributes, uploads: {}, token_yaml: nil)
+      current = @store.read
+      locales = Array(attributes[:supported_locales]).reject(&:blank?).uniq
+      raise ArgumentError, "At least one supported locale is required" if locales.empty?
+      raise ArgumentError, "Default locale must be supported" unless locales.include?(attributes[:default_locale])
+
+      payload = current.merge(
+        "application_name" => attributes[:application_name].to_s.strip,
+        "default_locale" => attributes[:default_locale],
+        "supported_locales" => locales
+      )
+      raise ArgumentError, "Application name is required" if payload["application_name"].blank?
+
+      if token_yaml.present?
+        raise ArgumentError, "Design token editing is disabled in production" unless Configuration.token_editing_allowed?
+
+        payload["design_tokens_sha256"] = @token_installer.install!(token_yaml)
+      end
+
+      %i[logo compact_logo favicon].each do |role|
+        path = @branding_store.store(uploads[role], role: role)
+        payload["#{role}_path"] = path if path
+      end
+
+      @store.write!(payload)
+    end
+  end
+end
