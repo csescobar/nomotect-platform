7e70cf44c75c066e2a7cf58edaa8b2cefadea00a
Cesar Escobar <cesarsilvaescobar@gmail.com>
2026-07-27 10:02:07 -0300

    feat: add protected installation controller

---
+++ app/controllers/installation/base_controller.rb
@@ -0,0 +1,31 @@
+module Installation
+  class BaseController < ApplicationController
+    allow_unauthenticated_access
+
+    before_action :ensure_enabled
+    before_action :ensure_incomplete
+    before_action :authorize_bootstrap
+
+    private
+
+    def ensure_enabled
+      head :not_found unless Configuration.enabled?
+    end
+
+    def ensure_incomplete
+      redirect_to root_path if StateStore.new.completed?
+    end
+
+    def authorize_bootstrap
+      return unless Configuration.production_token_required?
+      return if session[:installation_authorized]
+
+      token = BootstrapToken.new
+      if token.valid?(params[:token])
+        session[:installation_authorized] = true
+      else
+        head :forbidden
+      end
+    end
+  end
+end
