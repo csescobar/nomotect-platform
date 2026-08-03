3bab2c06afd77dfb03106bf9e58fb49c30c43f0d
Cesar Escobar <cesarsilvaescobar@gmail.com>
2026-08-03 12:01:28 -0300

    Keep test method stubbing self-contained

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,24 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
-require "minitest/mock"
+
+module TestMethodStubbing
+  def stub(method_name, replacement)
+    singleton = singleton_class
+    direct = (singleton.instance_methods(false) + singleton.private_instance_methods(false) + singleton.protected_instance_methods(false)).include?(method_name)
+    original = singleton.instance_method(method_name) if direct
+    singleton.define_method(method_name, replacement)
+    yield
+  ensure
+    if direct
+      singleton.define_method(method_name, original)
+    else
+      singleton.remove_method(method_name)
+    end
+  end
+end
+
+Module.prepend(TestMethodStubbing)
 
 module ActiveSupport
   class TestCase
